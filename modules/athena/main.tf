6e88dbb916012f80cf6255003cb63e35e95b5085
Sudheer Doppa <doppa.sudheer@gmail.com>
2025-09-22 23:08:54 +0530

    demo changes

--- modules/athena/main.tf
+++ modules/athena/main.tf
@@ -5,10 +5,4 @@ provider "aws" {
 resource "aws_athena_database" "cur_db" {
   name   = "${var.database_name_prefix}_${var.environment}"
   bucket = var.bucket_name
-
-  tags = {
-    Name        = "${var.database_name_prefix}_${var.environment}"
-    Environment = var.environment
-    Owner       = "sudheer"
-  }
 }
